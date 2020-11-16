3ea602bf5698e73e8d171cbfd2527872fb551aba
Fabio Duarte <37967244+lh536@users.noreply.github.com>
2020-11-16 15:44:04 -0500

    Se modificó el main.js para capturar los datos e imprimirlos en consola (#2)
    * Primer commit - Se configuró el proyecto inicial, se realizaron modificaciones al README.md con información importante. Se incluyó .gitigonre. Se realizó un primer ejemplo de formulario con un único campo y un botón para enviar para realizar pruebas simples
    
    * Se agregaron ids al html y se modificó el main.js para capturar los datos que se ingresen en el campo de texto e imprimirlos en consola
    
    Co-authored-by: Edwin Francisco Mancipe Gonzalez <71473659+efmg0325@users.noreply.github.com>

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,9 @@
-console.log('Hola Mundo!!');
+const botonEnviar = document.querySelector('#botonEnviar');
+const campoDato = document.querySelector('#campoDato');
+
+botonEnviar.addEventListener('click', (e) => {
+    e.preventDefault();
+    let contenidoCampoDato = campoDato.value;
+    console.log(`Se presionó el botón enviar y se envió: ${contenidoCampoDato}`);
+    campoDato.value = '';
+});
